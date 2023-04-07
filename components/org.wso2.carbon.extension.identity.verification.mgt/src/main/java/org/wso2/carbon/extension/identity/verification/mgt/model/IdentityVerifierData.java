GANGANI/identity-verification-1

Identity Verification

components/org.wso2.carbon.extension.identity.verification.mgt/src/main/java/org/wso2/carbon/extension/identity/verification/mgt/model/IdentityVerifierData.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.extension.identity.verification.mgt.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class contains the implementation of IdentityVerifierResponse.
+ */
+public class IdentityVerifierData {
+
+    String identityVerifierName;
+    List<IdVClaim> idVClaims;
+    List<IdVProperty> idVProperties;
+
+    public String getIdentityVerifierName() {
+
+        return identityVerifierName;
+    }
+
+    public void setIdentityVerifierName(String identityVerifierName) {
+
+        this.identityVerifierName = identityVerifierName;
+    }
+
+    public List<IdVClaim> getIdVClaims() {
+
+        return idVClaims;
+    }
+
+    public void setIdVClaims(List<IdVClaim> idVClaims) {
+
+        this.idVClaims = idVClaims;
+    }
+
+    public List<IdVProperty> getIdVProperties() {
+
+        return idVProperties;
+    }
+
+    public void setIdVProperties(List<IdVProperty> idVProperties) {
+
+        this.idVProperties = idVProperties;
+    }
+
+    public void addIdVClaimProperty(IdVClaim idVClaim) {
+
+        if (this.idVClaims == null) {
+            this.idVClaims = new ArrayList<>();
+        }
+        this.idVClaims.add(idVClaim);
+    }
+
+    public void addIdVProperty(IdVProperty idVProperty) {
+
+        if (idVProperties == null) {
+            idVProperties = new ArrayList<>();
+        }
+        idVProperties.add(idVProperty);
+    }
+}

components/org.wso2.carbon.extension.identity.verification.provider/src/main/java/org/wso2/carbon/extension/identity/verification/provider/model/IdentityVerificationProvider.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.extension.identity.verification.provider.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class contains the properties of the IdentityVerificationProvider.
+ */
+public class IdentityVerificationProvider {
+
+    private String id;
+    private String uuid;
+    private String idVProviderName;
+    private boolean enable;
+    private String idVProviderDescription;
+    private Map<String, String> claimMappings = new HashMap<>();
+    private IdVConfigProperty[] idVConfigProperties = new IdVConfigProperty[0];
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+    }
+
+    public String getIdVProviderName() {
+
+        return idVProviderName;
+    }
+
+    public void setIdVProviderName(String idVProviderName) {
+
+        this.idVProviderName = idVProviderName;
+    }
+
+    public IdVConfigProperty[] getIdVConfigProperties() {
+
+        return idVConfigProperties;
+    }
+
+    public void setIdVConfigProperties(IdVConfigProperty[] idVConfigProperties) {
+
+        this.idVConfigProperties = idVConfigProperties;
+    }
+
+    public void setIdVPUUID(String idVProviderId) {
+
+        this.uuid = idVProviderId;
+    }
+
+    public String getIdVPUUID() {
+
+        return uuid;
+    }
+
+    public boolean isEnable() {
+
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+
+        this.enable = enable;
+    }
+
+    public String getIdVProviderDescription() {
+
+        return idVProviderDescription;
+    }
+
+    public void setIdVProviderDescription(String idVProviderDescription) {
+
+        this.idVProviderDescription = idVProviderDescription;
+    }
+
+    public Map<String, String> getClaimMappings() {
+
+        return claimMappings;
+    }
+
+    public void setClaimMappings(Map<String, String> claimMappings) {
+
+        this.claimMappings = claimMappings;
+    }
+}

wso2-extensions/identity-verification

components/org.wso2.carbon.extension.identity.verification.mgt/src/main/java/org/wso2/carbon/extension/identity/verification/mgt/model/IdentityVerifierData.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.extension.identity.verification.mgt.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class contains the implementation of IdentityVerifierResponse.
+ */
+public class IdentityVerifierData {
+
+    String identityVerificationProviderId;
+    List<IdVClaim> idVClaims;
+    List<IdVProperty> idVProperties;
+
+    public String getIdentityVerificationProviderId() {
+
+        return identityVerificationProviderId;
+    }
+
+    public void setIdentityVerificationProviderId(String identityVerificationProviderId) {
+
+        this.identityVerificationProviderId = identityVerificationProviderId;
+    }
+
+    public List<IdVClaim> getIdVClaims() {
+
+        return idVClaims;
+    }
+
+    public void setIdVClaims(List<IdVClaim> idVClaims) {
+
+        this.idVClaims = idVClaims;
+    }
+
+    public List<IdVProperty> getIdVProperties() {
+
+        return idVProperties;
+    }
+
+    public void setIdVProperties(List<IdVProperty> idVProperties) {
+
+        this.idVProperties = idVProperties;
+    }
+
+    public void addIdVClaimProperty(IdVClaim idVClaim) {
+
+        if (this.idVClaims == null) {
+            this.idVClaims = new ArrayList<>();
+        }
+        this.idVClaims.add(idVClaim);
+    }
+
+    public void addIdVProperty(IdVProperty idVProperty) {
+
+        if (idVProperties == null) {
+            idVProperties = new ArrayList<>();
+        }
+        idVProperties.add(idVProperty);
+    }
+}

components/org.wso2.carbon.extension.identity.verification.provider/src/main/java/org/wso2/carbon/extension/identity/verification/provider/model/IdentityVerificationProvider.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.extension.identity.verification.provider.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class contains the properties of the IdentityVerificationProvider.
+ */
+public class IdentityVerificationProvider {
+
+    private String id;
+    private String uuid;
+    private String idVProviderName;
+    private boolean isEnabled = false;
+    private String idVProviderDescription;
+    private Map<String, String> claimMappings = new HashMap<>();
+    private IdVConfigProperty[] idVConfigProperties = new IdVConfigProperty[0];
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+    }
+
+    public String getIdVProviderName() {
+
+        return idVProviderName;
+    }
+
+    public void setIdVProviderName(String idVProviderName) {
+
+        this.idVProviderName = idVProviderName;
+    }
+
+    public IdVConfigProperty[] getIdVConfigProperties() {
+
+        return idVConfigProperties;
+    }
+
+    public void setIdVConfigProperties(IdVConfigProperty[] idVConfigProperties) {
+
+        this.idVConfigProperties = idVConfigProperties;
+    }
+
+    public void setIdVPUUID(String idVProviderId) {
+
+        this.uuid = idVProviderId;
+    }
+
+    public String getIdVProviderUuid() {
+
+        return uuid;
+    }
+
+    public boolean isEnabled() {
+
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+
+        this.isEnabled = enabled;
+    }
+
+    public String getIdVProviderDescription() {
+
+        return idVProviderDescription;
+    }
+
+    public void setIdVProviderDescription(String idVProviderDescription) {
+
+        this.idVProviderDescription = idVProviderDescription;
+    }
+
+    public Map<String, String> getClaimMappings() {
+
+        return claimMappings;
+    }
+
+    public void setClaimMappings(Map<String, String> claimMappings) {
+
+        this.claimMappings = claimMappings;
+    }
+}